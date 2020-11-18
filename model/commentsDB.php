371df386fed95e2386da62e8dece5e7df2e53b56
nivo258 <nivo258@freemail.hu>
2020-11-18 11:13:06 +0100

    comments fügvények

--- model/commentsDB.php
+++ model/commentsDB.php
@@ -3,11 +3,13 @@ require "db.php";
 
 class commentsDB extends db
 {
-    function createComment(){
-
+    function createComment($fileID,$commenterID,$comment){
+        $this->select("INSERT INTO `comments`(`FileID`, `CommenterID`, `Content`) VALUES (".$fileID.",".$commenterID.",'".$comment."');");
+        $this->log($fileID."-hez hozzászolás érkezett ".$commenterID."-től");
     }
-    function deleteComment(){
-
+    function deleteComment($ID){
+        $this->select("DELETE FROM `comments` WHERE ID = ".$ID."; ");
+        $this->log($ID."-as komment törölve lett");
     }
 
 }
