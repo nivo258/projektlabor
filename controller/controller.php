e1c0f540f4e1fe414273f9ffd269c7776e53a4c2
nivo258 <nivo258@freemail.hu>
2020-12-07 20:20:22 +0100

    fix

--- controller/controller.php
+++ controller/controller.php
@@ -18,15 +18,14 @@ $res = $asd-> login($username, $password);
 
 if($res[0]['code']== 200)
 {
-	$_SESSION['userlvl'] = $asd->getLVL($username);
 	$_SESSION['user_logged_in']= TRUE;
 	$_SESSION['username'] = $username;
-	header('Location:../view/main.php');
+	header('Location: ../view/main.php');
 }
 
 else 
 {
 	$_SESSION['login_failure'] = "Hibás felhasználónév vagy jelszó";
-	header('Location:../view/index.php');
+	header('Location: ../view/index.php');
 }
  ?>
