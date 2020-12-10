9ed292d069b585c385088895d59df838bf7317b2
nivo258 <nivo258@freemail.hu>
2020-12-10 09:05:49 +0100

    Update log.php

--- controller/log.php
+++ controller/log.php
@@ -1,4 +1,35 @@
 <?php
-    mysql_connect('localhost','admin','admin');
-    mysql_select_db("projektlabor");
+require "../model/logDB.php";
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$asd = new logDB();
+
+$res = $asd->listlog();
+
 ?>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Dátum</th>
+        <th>Leírás</th>
+
+    </tr>
+    <?php
+    for ($i=0;$i<count($res);$i++) {
+        ?>
+
+        <tr>
+            <td><?php echo $res[$i]['ID']; ?></td>
+            <td><?php echo $res[$i]['Date']; ?></td>
+            <td><?php echo $res[$i]['Log']; ?></td>
+        </tr>
+
+
+        <?php
+    }
+    ?>
+</table>
